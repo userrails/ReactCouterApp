f42b89d4684c14de9b93f561cead7ebb2b79c041
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-19 16:15:35 +0545

    App component called on index and app.css added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css'
+
+class App extends React.Component {
+	render() {
+		return(
+			<div className="app-component"><h2>hello react!!</h2></div>
+	  );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 ReactDOM.render(
-	<div>Hello React</div>, document.getElementById("root")
+	<App />, document.getElementById("root")
 );
 
