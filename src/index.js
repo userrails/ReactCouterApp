4a922f870b0c5f80f17fd615232b02cd7c55ed7e
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-19 17:14:21 +0545

    small changes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	<App name="Shiv raj" email="shivrajbadu@gmail.com" />, document.getElementById("root")
+	<App name="Shiv raj" email="shivraj@abc.xyzz" />, document.getElementById("root")
 );
