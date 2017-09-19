4bb8a1592092e6569249ca9d1bcc44197fcb990f
Shiv Raj Badu <shivrajbadu@gmail.com>
2017-09-19 16:59:03 +0545

    Implementing counter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import React from 'react';
 import './App.css'
 
 class App extends React.Component {
-	render() {
-		return(
-			<div className="app-component"><h2>hello react!! {this.props.name} / {this.props.email}</h2></div>
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+
+  increment() {
+    console.log(this)
+    // this.setState({count: this.state.count + 1});
+  }
+
+  render() {
+    return(
+      <div className="app-component">
+        <h2>hello react!! {this.props.name}
+         / {this.props.email}</h2>
+         {console.log(this)}
+        <div>
+          <p>Count: {this.state.count}</p>
+          <p><button onClick={this.increment}>Click!</button></p>
+        </div>
+      </div>
 	  );
   }
 }
